ed35d905606936c385e3c0ada54a88fc9540143c
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2024-11-08 14:31:57 -0600

    chore(version): bump version to 1.0.9

--- react.js
+++ react.js
@@ -1,10 +1,5 @@
-import { existsSync } from 'node:fs';
-
 import pluginReact from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
-
-/** Helper function to conditionally apply paths */
-const applyIfExists = (path, config) => (existsSync(path) ? config : undefined);
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -23,32 +18,4 @@ export default [
       'react/prop-types': 'off', // Prevent missing props validation in a React component definition
     },
   },
-  // Conditionally apply these configurations based on path existence
-  applyIfExists('src/components', {
-    files: ['src/components/**/*.{js,jsx,ts,tsx}'],
-    rules: {
-      'unicorn/filename-case': [
-        'error',
-        { cases: { pascalCase: true } }, // PascalCase for components and pages
-      ],
-    },
-  }),
-  applyIfExists('src/hooks', {
-    files: ['src/hooks/**/*.{js,ts}'],
-    rules: {
-      'unicorn/filename-case': [
-        'error',
-        { cases: { camelCase: true } }, // camelCase for hooks
-      ],
-    },
-  }),
-  applyIfExists('src/utils', {
-    files: ['src/utils/**/*.{js,ts}'],
-    rules: {
-      'unicorn/filename-case': [
-        'error',
-        { cases: { camelCase: true } }, // camelCase for utils
-      ],
-    },
-  }),
-].filter(Boolean); // Remove any `null` entries from the array
+];
--- types.d.ts
+++ types.d.ts
@@ -1,29 +1,29 @@
 declare module 'eslint-config-zl-asica' {
   import type { Linter } from 'eslint';
 
-  export const zlAsicaConfig: Linter.Config,
-    zlAsicaReactConfig: Linter.Config,
-    zlAsicaTsConfig: Linter.Config;
+  export const zlAsicaConfig: Linter.Config[],
+    zlAsicaReactConfig: Linter.Config[],
+    zlAsicaTsConfig: Linter.Config[];
 }
 
 // For direct imports if needed
 declare module 'eslint-config-zl-asica/base.js' {
   import type { Linter } from 'eslint';
 
-  const baseConfig: Linter.Config[];
+  const baseConfig: Linter.Config;
   export default baseConfig;
 }
 
 declare module 'eslint-config-zl-asica/typescript.js' {
   import type { Linter } from 'eslint';
 
-  const tsConfig: Linter.Config[];
+  const tsConfig: Linter.Config;
   export default tsConfig;
 }
 
 declare module 'eslint-config-zl-asica/react.js' {
   import type { Linter } from 'eslint';
 
-  const reactConfig: Linter.Config[];
+  const reactConfig: Linter.Config;
   export default reactConfig;
 }
